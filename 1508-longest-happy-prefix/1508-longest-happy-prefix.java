d76dd23ca63e84ea8bf69237d21857f6810130d7
Agnik Chakraborty <122608231+agnikc21@users.noreply.github.com>
2025-04-08 20:39:18 +0530

    Time: 1327 ms (21.76%), Space: 47.3 MB (9.16%) - LeetHub

--- 1508-longest-happy-prefix/1508-longest-happy-prefix.java
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.java
@@ -1,12 +1,11 @@
 class Solution {
     public String longestPrefix(String s) {
-        String res = "";
         for (int i= s.length()-1; i>0;i--){
             String str = s.substring(0,i);
             if (s.endsWith(str)){
                 return str;
             }
         }
-        return res;
+        return "";
     }
 }
