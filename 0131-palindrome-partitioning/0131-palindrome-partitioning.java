1121f57f8a310cc9cb87b154114cfd39acdeecec
Agnik Chakraborty <122608231+agnikc21@users.noreply.github.com>
2025-07-02 22:02:08 +0530

    Time: 8 ms (53.86%), Space: 57 MB (44.38%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        backtrack(s, new ArrayList<>(), ans, 0);
+        return ans;
+    }
+
+    private void backtrack(String s, List<String> curr, List<List<String>> ans, int indx){
+        if (indx==s.length()){
+            ans.add(new ArrayList<>(curr));
+            return;
+        } 
+
+        for (int i = indx+1; i<=s.length();i++ ){
+            String str= s.substring(indx,i);
+            if (isPali(str)){
+                curr.add(str);
+                backtrack(s,curr,ans,i);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+    private boolean isPali(String s){
+        int l=0;
+        int r=s.length()-1;
+        while (l<=r){
+            if (s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
