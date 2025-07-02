1ee0d15a2e8653894d9a04ea1423757caf62ec4e
Agnik Chakraborty <122608231+agnikc21@users.noreply.github.com>
2025-07-02 22:03:12 +0530

    Time: 7 ms (97.78%), Space: 57.1 MB (31.1%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -23,11 +23,8 @@ class Solution {
     private boolean isPali(String s){
         int l=0;
         int r=s.length()-1;
-        while (l<=r){
-            if (s.charAt(l)==s.charAt(r)){
-                l++;
-                r--;
-            } else{
+        while (l<r){
+            if (s.charAt(l++)!=s.charAt(r--)){
                 return false;
             }
         }
